229c836c6c15016fc1b8b453a3772aeb70e02f3e
hagai luger <hagai.luger@gmail.com>
2022-03-08 09:57:36 +0200

    Add files via upload

---
+++ MyEmployees.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningLinq
+{
+	public class Employee
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public string Gender { get; set; }
+		public int Salary { get; set; }
+
+		public Employee(string name, int age, string gender, int salary)
+		{
+			Name = name;
+			Age = age;
+			Gender = gender;
+			Salary = salary;
+		}
+
+	}
+
+	public class MyEmployees
+	{
+		public static List<Employee> GetEmployees()
+		{
+			return new List<Employee>
+			{
+				new Employee("hagai",43,"Male",30000),
+				new Employee("dana",25,"Female",32000),
+				new Employee("shuki",20,"Male",18000),
+				new Employee("avi",45,"Male",20000),
+				new Employee("michal",40,"Female",28000),
+			};
+		}
+	}
+}
